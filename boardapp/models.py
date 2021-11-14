bb39cd919f9d04ee86a959c21d9962da9a9df5f3
shohma <studious0404@gmail.com>
2021-11-14 14:37:51 +0900

    migration1

--- boardapp/models.py
+++ boardapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.fields import CharField,TextField,IntegerField
 
 # Create your models here.
+
+CHOICE = (('primary','high'),('primary','normal'),('primary','low'))
+
+class BoadModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    contributor = models.CharField(max_length=20)
+    good = models.IntegerField()
+    priority = models.CharField(max_length=10,choices=CHOICE)
+
+    def __str__(self):
+        return self.title
+    
