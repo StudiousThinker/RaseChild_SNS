f02204983b66a8988964bebecd972774444323c9
shohma <studious0404@gmail.com>
2021-11-14 12:39:00 +0900

    making login 1

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -8,6 +10,30 @@ def signupfunc(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        user = User.objects.create_user(username,'',password)
+
+        try:
+            user = User.objects.create_user(username,'',password)
+            return render(request, 'signup.html', {})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error':'このユーザーは既に登録されています'})
 
     return render(request, 'signup.html', {})
+
+def loginfunc(request):
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+            
+        if user is not None:
+            login(request,user)
+            return render(request, 'login.html', {'context':'Loged in'})
+        else:
+            return render(request, 'login.html', {'context':'このユーザーは登録されていません'})
+
+    return render(request, 'login.html', {'context':'get method'})
+
+
+
+
