0596465df0c615e214a3b03633792a8a9a218b2e
shohma <studious0404@gmail.com>
2021-11-18 23:11:24 +0900

    Linting

--- boardapp/models.py
+++ boardapp/models.py
@@ -1,18 +1,15 @@
 from django.db import models
-from django.db.models.fields import CharField,TextField,IntegerField
 
-# Create your models here.
+CHOICE = (('danger', 'high'), ('success', 'normal'), ('primary', 'low'))
 
-CHOICE = (('danger','high'),('success','normal'),('primary','low'))
 
 class BoadModel(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     contributor = models.CharField(max_length=20)
     good = models.IntegerField(null=True, blank=True, default=1)
-    priority = models.CharField(max_length=10,choices=CHOICE)
+    priority = models.CharField(max_length=10, choices=CHOICE)
     follow = models.IntegerField(null=True, blank=True, default=0)
 
     def __str__(self):
         return self.title
-    
--- boardapp/views.py
+++ boardapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.db import IntegrityError, models
+from django.db import IntegrityError
 from .models import BoadModel
 from django.contrib.auth.decorators import login_required
-from django.views.generic import CreateView,UpdateView,DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
@@ -13,34 +13,38 @@ def signupfunc(request):
         username = request.POST['username']
         password = request.POST['password']
         try:
-            user = User.objects.create_user(username,'',password)
+            User.objects.create_user(username, '', password)
             return render(request, 'signup.html', {})
         except IntegrityError:
-            return render(request, 'signup.html', {'error':'このユーザーは既に登録されています'})
-    return render(request, 'signup.html', {'context':'get method'})
+            return render(request, 'signup.html',
+                          {'error': 'このユーザーは既に登録されています'}
+                          )
+    return render(request, 'signup.html', {'context': 'get method'})
 
 
-def loginfunc(request): 
+def loginfunc(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)        
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request,user)
+            login(request, user)
             return redirect('list')
         else:
-            return render(request, 'login.html', {'context':'ユーザー名もしくはパスワードが間違っています'})
+            return render(request, 'login.html',
+                          {'context': 'ユーザー名もしくはパスワードが間違っています'}
+                          )
     return render(request, 'login.html', {})
 
 
 @login_required
-def listfunc(request): 
+def listfunc(request):
     object_list = BoadModel.objects.all()
     if request.path_info == '/list/':
         html = 'list.html'
     else:
         html = 'mylist.html'
-    return render(request, html, {'object_list':object_list})
+    return render(request, html, {'object_list': object_list})
 
 
 def logoutfunc(request):
@@ -50,33 +54,42 @@ def logoutfunc(request):
 
 def detailfunc(request, pk):
     object = get_object_or_404(BoadModel, pk=pk)
-    return render(request, 'detail.html',{'object':object})
+    return render(request, 'detail.html', {'object': object})
 
-def goodfunc(request, pk): 
+
+def goodfunc(request, pk):
     object = get_object_or_404(BoadModel, pk=pk)
     object.good = object.good + 1
     object.save()
     return redirect('list')
 
+
 class ContributeCreate(CreateView):
     template_name = 'create.html'
     model = BoadModel
-    fields = ('title','content','contributor','priority')
+    fields = ('title', 'content', 'contributor', 'priority')
     success_url = reverse_lazy('list')
+
 
 class ContributeDelete(DeleteView):
     template_name = 'delete.html'
     model = BoadModel
     success_url = reverse_lazy('mylist')
 
+
 class ContributeUpdate(UpdateView):
     template_name = 'update.html'
     model = BoadModel
-    fields = ('title','content','priority')
+    fields = ('title', 'content', 'priority')
     success_url = reverse_lazy('mylist')
+
 
 def followfunc(request, pk):
     object = get_object_or_404(BoadModel, pk=pk)
-    dummy = BoadModel.objects.create(title=object.title,content=object.content,contributor=request.user.username,priority=object.priority,follow=1)
+    dummy = BoadModel.objects.create(title=object.title,
+                                     content=object.content,
+                                     contributor=request.user.username,
+                                     priority=object.priority,
+                                     follow=1)
     dummy.save()
     return redirect('mylist')
